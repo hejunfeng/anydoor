9e5d0115c2f4e8b52bcc964392b083b5758515bb
kaka19881111 <hejunfeng1090@163.com>
2018-04-09 00:28:39 +0800

    promise

--- src/app.js
+++ src/app.js
@@ -2,28 +2,37 @@ const http = require('http');
 const chalk = require('chalk');
 const path =require('path');
 const fs = require('fs');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
 const conf = require('./config/defaultConfig');
 
 const server = http.createServer((req, res)=>{
 	const filePath = path.join(conf.root,req.url);
-	fs.stat(filePath,(err,stats)=>{
-		if(err){
+	try{
+		stat(filePath).then(stats=>{
+			if (stats.isFile()){
+				res.statusCode =200;
+				res.setHeader('Content-Type','text/plain');
+				fs.createReadStream(filePath).pipe(res);
+			}else if(stats.isDirectory()){
+				readdir(filePath).then((files)=>{
+					res.statusCode =200;
+					res.setHeader('Content-Type','text/plain');
+					res.end(files.join(','));
+				});
+			}
+		}).catch(err=>{
 			res.statusCode = 404;
 			res.setHeader('Content-Type','text/plain');
-			res.end('is not a directory');
-			return;
-		}
-		if (stats.ifFile()){
-			res.statusCode =200;
-			res.setHeader('Content-Type','text/plain');
-			fs.createReadStream(filePath).pipt(res);
-		}
-	});
-
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/html');
-	res.end(filePath);
-	
+			res.end(err.statusCode);
+		});
+	} 
+	catch(ex){
+		res.statusCode = 404;
+		res.setHeader('Content-Type','text/plain');
+		res.end('is not a directory');
+	}
 });
 
 server.listen(conf.port,conf.hostname,()=>{
